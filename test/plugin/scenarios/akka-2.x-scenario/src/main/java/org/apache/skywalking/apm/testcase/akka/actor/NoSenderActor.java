65b3a4132e332d744ef7ba0c9147c88493280499
vcjmhg <vcjmhg@163.com>
2021-01-04 21:49:16 +0800

    polish Akka-scenario

---
+++ test/plugin/scenarios/akka-2.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/akka/actor/NoSenderActor.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.apm.testcase.akka.actor;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class NoSenderActor extends AbstractActor {
+    static public Props props() {
+        return Props.create(NoSenderActor.class, NoSenderActor::new);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("start", msg -> {
+                    log.info("NoSenderActor received:" + msg + "!!!");
+                    getSender().tell("from [noSenderActor]", ActorRef.noSender());
+                }).matchEquals("stop", msg -> {
+                    log.fatal("NoSender Actor will be stopped because received signal!!");
+                    getContext().stop(getSelf());
+                }).matchAny(msg -> {
+                    log.info("NoSender has received: " + msg + " from " + getSender());
+                }).build();
+    }
+}
